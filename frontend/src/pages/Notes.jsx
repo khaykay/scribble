aa42273f77da4ac3936dad872c6040d9fd2cc777
khaykay <berryscott97@gmail.com>
2024-09-27 14:20:24 +0100

    added responsiveness to the note component

--- frontend/src/pages/Notes.jsx
+++ frontend/src/pages/Notes.jsx
@@ -5,21 +5,24 @@ import NewNote from "./newNote";
 const Notes = () => {
   const { notes } = useNotes();
   return (
-    <div className="px-4">
-      <header>My Notes</header>
-      <nav className="list-none flex  ">
-        <li className="border border-solid border-slate-700 rounded-3xl p-3 basis-1/4 ">
-          All
+    <div className="px-4 flex flex-col gap-y-6 md:grid md:grid-cols-[200px_minmax(900px,_1fr)] md:grid-rows-1 md: gap-x-5 md:gap-y-0">
+      <header className="text-6xl md:col-start-2">My Notes</header>
+      <nav className="list-none flex gap-2 md:flex-col ">
+        <li className="border border-solid border-slate-700 rounded-3xl p-3 ">
+          All{" "}
+          <span className="border border-solid border-slate-700 rounded-full h-7 w-7 inline-block text-center bg-slate-700 text-slate-300 ">
+            {notes.length}
+          </span>
         </li>
-        <li className="border border-solid border-slate-700 rounded-3xl p-3 basis-1/4">
+        <li className="border border-solid border-slate-700 rounded-3xl p-3 ">
           Important
         </li>
-        <li className="border border-solid border-slate-700 rounded-3xl p-3 basis-1/4">
+        <li className="border border-solid border-slate-700 rounded-3xl p-3 ">
           Folders
         </li>
       </nav>
 
-      <div className="flex h-56 basis-1/2 gap-4 ">
+      <div className="flex gap-4 md:col-start-2">
         {notes.map((note, index) => (
           <div
             key={index}
@@ -27,7 +30,7 @@ const Notes = () => {
               index % 2 === 0
                 ? "rounded-tl-none rounded-tr-3xl rounded-b-3xl"
                 : "rounded-t-3xl rounded-bl-3xl rounded-br-none"
-            } p-3`}
+            } p-3 basis-1/2  h-56 `}
           >
             <h3>title: {note.title}</h3>
             <p>content: {note.content}</p>
